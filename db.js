03d745de945c821c9e97ffff5810cfb46384c8d8
valugit <44845228+valugit@users.noreply.github.com>
2020-07-01 00:33:48 +0200

    Added db_port && db_host in .env

--- db.js
+++ db.js
@@ -4,9 +4,12 @@ const { Sequelize, DataTypes } = require('sequelize')
 
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_PORT = process.env.DB_PORT || 3306
 
 const sequelize = new Sequelize('paint', DB_USER, DB_PASSWORD, {
-  host: 'localhost',
+  host: DB_HOST,
+  port: DB_PORT,
   dialect: 'mysql'
 })
 
